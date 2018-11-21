67a304842d3667bca986cdb661f2dd2510cd001b
stevannohero <stevannohero@gmail.com>
2018-11-21 22:42:36 +0700

    Update schedule and input

--- src/Schedule.java
+++ src/Schedule.java
@@ -1,22 +1,8 @@
 public class Schedule {
     public String courseID;
     public String roomID;
-    public DayTime dayTime;
-    public Schedule(String courseID, String roomID, DayTime dayTime) {
+    public Schedule(String courseID, String roomID) {
         this.courseID = courseID;
         this.roomID = roomID;
-        this.dayTime = dayTime;
-    }
-
-    public boolean isAvailable(DayTime time) {
-        if (time.day != dayTime.day) {
-            return true;
-        }
-        int i = 0;
-        for (Time hour : dayTime.times) {
-            if (hour.from <= time.times.get(i).from) {
-
-            }
-        }
     }
 }
